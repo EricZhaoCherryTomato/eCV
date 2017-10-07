373f88c475f5fc5f62ba363e6a83ae700b598b01
Eric Zhao <eric.ye.zhao@gmail.com>
2017-10-07 16:59:45 +1000

    test remove

--- CVOnline/src/app/app.component.ts
+++ CVOnline/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Eric Zhao e-Resume';
-  note = 'Test';
+  //note = 'Test';
 
    private itemsList:Object[] = [
         {
