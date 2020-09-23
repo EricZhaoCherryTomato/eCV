d61d8794d6b0e63b10100ac9f7cb6079642bc386
EricZ <eric.ye.zhao@gmail.com>
2020-09-23 10:12:03 +1000

    fix lint error

--- CVOnline/src/app/app.component.ts
+++ CVOnline/src/app/app.component.ts
@@ -7,20 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Test e-Resume';
-  //note = 'I am here with the brilliant guys at TSWG!';
 
    private itemsList:Object[] = [
         {
           title: 'Jan 2015 - Test One',
-          projects: [ { name : 'App', tech: 'Angular, TypeScript, Gulp, AWS, C# Web Api, Autofac DI'},
-                      { name : 'Project', tech: 'KnockoutJS, AJAX, CSS'},
+          projects: [ 
+                      { name : 'App', tech: 'Angular, TypeScript, Gulp, AWS, C# Web Api, Autofac DI'},
+                      { name : 'Project', tech: 'KnockoutJS, AJAX, CSS'}
+                    ]
         {
           title: 'Mar 2014 - Jan 2015 Test Two',
-          projects: [ { name : 'Internal and Public website', tech: 'JQuery HTML5'},
+          projects: [ 
+                      { name : 'Internal and Public website', tech: 'JQuery HTML5'}
+                    ]
         },
         {
           title: 'Dec 2012 - Mar 2014   Test Three',
-          projects: [ { name : 'Smart App', tech: 'VS2010, C#'}
+          projects: [ 
+                      { name : 'Smart App', tech: 'VS2010, C#'}
                     ]
         }
     ]
